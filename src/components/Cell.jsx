e0e7b13f472d0bc4c6d2bf74f4cb2774c52f1160
Jose Blondel <164395005+Chijopana@users.noreply.github.com>
2025-04-25 19:47:25 +0200

    ahora se ve el barco del jugador  - Proyecto de Battleship

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,31 +1,29 @@
-// Cell.js
 import React from 'react';
 
 const Cell = ({ cell, onClick, isPlayer }) => {
   let cellStyle = '';
 
-  // Si es el tablero del jugador
+
   if (isPlayer) {
     if (cell.hasShip) {
-      // Barcos visibles en gris cuando no son tocados
-      cellStyle = cell.hit ? 'bg-red-500' : 'bg-gray-400'; // Barcos en gris
+
+      cellStyle = cell.hit ? 'bg-red-500' : 'bg-gray-400'; 
     } else {
-      // Agua, se pone azul si no ha sido tocado
-      cellStyle = cell.hit ? 'bg-blue-500' : 'bg-blue-300'; // Agua azul
+
+      cellStyle = cell.hit ? 'bg-blue-500' : 'bg-blue-300'; 
     }
   } else {
-    // Si es el tablero del enemigo (bot)
     if (cell.hit) {
       if (cell.hasShip) {
-        // Rojo si el bot tocó un barco
+ 
         cellStyle = 'bg-red-500';
       } else {
-        // Azul claro si el bot falló (agua)
+  
         cellStyle = 'bg-blue-500';
       }
     } else {
-      // Casillas no tocadas (gris por defecto)
-      cellStyle = 'bg-blue-300'; // Gris para las casillas no tocadas
+
+      cellStyle = 'bg-blue-300'; 
     }
   }
 
